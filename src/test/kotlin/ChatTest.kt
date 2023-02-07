1fef7ae141007e3bf86cb0d5eda60eba9db48bf9
nmak1 <nmak1@rambler.ru>
2023-02-07 08:40:08 +0300

    added ChatTest.kt

--- src/test/kotlin/ChatTest.kt
+++ src/test/kotlin/ChatTest.kt
@@ -12,12 +12,23 @@ class ChatTest {
     }
 
     @Test
-    fun deleteMessegesTest() {
+    fun deleteMessagesTest() {
         val chat = Chat()
         chat.addMessage("test")
         chat.addMessage("test 2")
         chat.deleteMesseges(0)
         Assert.assertEquals(chat.listMessage.size, 1)
+    }
+
+    @Test
+    fun readMessagesTest() {
+        val chat = Chat()
+        val message = Message()
+        chat.addMessage("test")
+        chat.addMessage("test 2")
+        chat.readMessages()
+        Assert.assertEquals(chat.listMessage.size, 2)
+        Assert.assertFalse(message.isRead)
     }
 
 }
